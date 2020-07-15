8916deb18cded9c70bf50e4162724440d48deb5e
Nick Kim <nicholaskim1989@gmail.com>
2020-07-14 18:39:51 -0700

    updated routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -67,10 +67,12 @@ module.exports = function(app) {
     });
   });
 
-  // Route for displaying all cards of a single topic
+  // Route for displaying all cards of a single topic !!!!!!!!!
   app.get("/api/topics/:topic/cards", (req, res) => {
     db.Card.findAll({
-      topicId: req.params.topic
+      where: {
+        topicId: req.params.topic
+      }
     }).then(dbCard => {
       res.json(dbCard);
     });
